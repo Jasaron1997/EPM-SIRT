a7742d70d1d81ae0899550477c5605412e818fb7
Jasaron1997 <jsotoc2@miumg.edu.gt>
2023-05-13 05:38:25 -0600

    getAutomatico

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -74,7 +74,7 @@ function App() {
 
   const getAutomatico = useCallback(() => {
     setAutomatico(!automatico);
-  }, []);
+  }, [automatico]);
 
   return (
     <div className="container-fluid">
